b57d65c392db4254c53eb3f0e63a9e54168c8683
torinunna <torinunna9588@gmail.com>
2023-07-01 23:39:43 +0900

    Initial Commit

---
+++ BookReview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookReview
+//
+//  Created by YUJIN KWON on 2023/07/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
