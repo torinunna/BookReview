dcbf6ac28e14acbf2ab18414a87351413bd43019
torinunna <torinunna9588@gmail.com>
2023-07-02 17:05:28 +0900

    add tableView to ReviewListVC and set datasource using presenter

--- BookReview/Scene/ReviewList/ReviewListPresenter.swift
+++ BookReview/Scene/ReviewList/ReviewListPresenter.swift
@@ -5,13 +5,14 @@
 //  Created by YUJIN KWON on 2023/07/01.
 //
 
-import Foundation
+import UIKit
 
 protocol ReviewListProtocol {
     func setUpNavigationBar()
+    func setUpViews()
 }
 
-final class ReviewListPresenter {
+final class ReviewListPresenter: NSObject {
     private let viewController: ReviewListProtocol
     
     init(viewController: ReviewListProtocol) {
@@ -20,6 +21,23 @@ final class ReviewListPresenter {
     
     func viewDidLoad() {
         viewController.setUpNavigationBar()
+        viewController.setUpViews()
     }
+    
+}
+
+extension ReviewListPresenter: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
+        cell.textLabel?.text = "\(indexPath)"
+        return cell
+    }
+    
+   
+    
     
 }
--- BookReview/Scene/ReviewList/ReviewListViewController.swift
+++ BookReview/Scene/ReviewList/ReviewListViewController.swift
@@ -4,12 +4,18 @@
 //
 //  Created by YUJIN KWON on 2023/07/01.
 //
-
+import SnapKit
 import UIKit
 
 final class ReviewListViewController: UIViewController {
     
     private lazy var presenter = ReviewListPresenter(viewController: self)
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.dataSource = presenter
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,5 +31,12 @@ extension ReviewListViewController: ReviewListProtocol {
         
         let rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: nil)
         navigationItem.rightBarButtonItem = rightBarButtonItem
+    }
+    
+    func setUpViews() {
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
 }
