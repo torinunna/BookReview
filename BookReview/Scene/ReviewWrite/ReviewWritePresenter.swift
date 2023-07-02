38bbc32e27fe3c09317edda4f7071fcc1106a8d1
torinunna <torinunna9588@gmail.com>
2023-07-02 17:11:55 +0900

    create ReviewWriteVC and Presenter in ReviewWrite

---
+++ BookReview/Scene/ReviewWrite/ReviewWritePresenter.swift
@@ -0,0 +1,12 @@
+//
+//  ReviewWritePresenter.swift
+//  BookReview
+//
+//  Created by YUJIN KWON on 2023/07/02.
+//
+
+import Foundation
+
+final class ReviewWritePresenter {
+    
+}
---
+++ BookReview/Scene/ReviewWrite/ReviewWriteViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ReviewWriteViewController.swift
+//  BookReview
+//
+//  Created by YUJIN KWON on 2023/07/02.
+//
+
+import SnapKit
+import UIKit
+
+final class ReviewWriteViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
