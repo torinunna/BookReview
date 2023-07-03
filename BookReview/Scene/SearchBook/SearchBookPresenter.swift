8be9af2a3c4aa65bd5484582467ff0e01fcc916e
torinunna <torinunna9588@gmail.com>
2023-07-03 22:25:44 +0900

    add tableView to SearchBookVC

--- BookReview/Scene/SearchBook/SearchBookPresenter.swift
+++ BookReview/Scene/SearchBook/SearchBookPresenter.swift
@@ -26,3 +26,17 @@ final class SearchBookPresenter: NSObject {
 
 extension SearchBookPresenter: UISearchBarDelegate {
 }
+
+extension SearchBookPresenter: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
+}
+
+extension SearchBookPresenter: UITableViewDelegate{
+    
+}
--- BookReview/Scene/SearchBook/SearchBookViewController.swift
+++ BookReview/Scene/SearchBook/SearchBookViewController.swift
@@ -5,11 +5,19 @@
 //  Created by YUJIN KWON on 2023/07/03.
 //
 
+import SnapKit
 import UIKit
 
 final class SearchBookViewController: UIViewController {
     
     private lazy var presenter = SearchBookPresenter(viewController: self)
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.dataSource = presenter
+        tableView.delegate = presenter
+        return tableView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,5 +32,10 @@ extension SearchBookViewController: SearchBookProtocol {
         searchController.obscuresBackgroundDuringPresentation = false
         searchController.searchBar.delegate = presenter
         navigationItem.searchController = searchController
+        
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
 }
