1a9e0b3fa7f0617f81d1b4a1c78b44f0ed0b296f
torinunna <torinunna9588@gmail.com>
2023-07-25 00:36:22 +0900

    rename BookReviewTests to ReviewListPresenterTests

--- BookReviewTests/ReviewListPresenterTests.swift
+++ BookReviewTests/ReviewListPresenterTests.swift
@@ -1,5 +1,5 @@
 //
-//  BookReviewTests.swift
+//  ReviewListPresenterTests.swift
 //  BookReviewTests
 //
 //  Created by YUJIN KWON on 2023/07/01.
@@ -8,7 +8,7 @@
 import XCTest
 @testable import BookReview
 
-class BookReviewTests: XCTestCase {
+class ReviewListPresenterTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
