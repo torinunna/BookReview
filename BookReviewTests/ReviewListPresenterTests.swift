da5b311b7ecbe96b0c8f43a478971bc0f971cb05
torinunna <torinunna9588@gmail.com>
2023-07-25 02:14:16 +0900

    set up ReviewListPresenterTest with a mock to test ReviewListPresenter

--- BookReviewTests/ReviewListPresenterTests.swift
+++ BookReviewTests/ReviewListPresenterTests.swift
@@ -10,27 +10,44 @@ import XCTest
 
 class ReviewListPresenterTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    var sut: ReviewListPresenter!
+    var viewController: MockReviewListViewController!
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func setUp() {
+        super.setUp()
+        
+        viewController = MockReviewListViewController()
+        sut = ReviewListPresenter(viewController: viewController)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    override func tearDown() {
+        sut = nil
+        viewController = nil
+        
+        super.tearDown()
     }
+}
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+final class MockReviewListViewController: ReviewListProtocol {
+    
+    var isSetUpNavigationBarCalled = false
+    func setUpNavigationBar() {
+        isSetUpNavigationBarCalled = true
     }
-
+    
+    var isSetUpViewsCalled = false
+    func setUpViews() {
+        isSetUpViewsCalled = true
+    }
+    
+    var isPresentReviewWriteVCCalled = false
+    func presentReviewWriteVC() {
+        isPresentReviewWriteVCCalled = true
+    }
+    
+    var isReloadTableViewCalled = false
+    func reloadTableView() {
+        isReloadTableViewCalled = true
+    }
+    
 }
