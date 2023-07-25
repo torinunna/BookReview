5896203efa2b1e35748238a22833409cfd618dea
torinunna <torinunna9588@gmail.com>
2023-07-25 15:09:47 +0900

    create ReviewWritePresenterTests

---
+++ BookReviewTests/Test/ReviewWritePresenterTests.swift
@@ -0,0 +1,14 @@
+//
+//  ReviewWritePresenterTests.swift
+//  BookReviewTests
+//
+//  Created by YUJIN KWON on 2023/07/25.
+//
+
+import XCTest
+@testable import BookReview
+
+class ReviewWritePresenterTests: XCTestCase {
+    
+
+}
