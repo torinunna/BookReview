3d9091fbbeb82ef9b7d32ba67582a8120624471b
torinunna <torinunna9588@gmail.com>
2023-07-25 15:33:23 +0900

    set up ReviewWritePresenterTest with mock

--- BookReviewTests/Test/ReviewWritePresenterTests.swift
+++ BookReviewTests/Test/ReviewWritePresenterTests.swift
@@ -9,6 +9,24 @@ import XCTest
 @testable import BookReview
 
 class ReviewWritePresenterTests: XCTestCase {
+    var sut: ReviewWritePresenter!
+    var viewController: MockReviewWriteViewController!
+    var userDefaultsMangaer: MockUserDefaultsManager!
     
+    override func setUp() {
+        super.setUp()
+        viewController = MockReviewWriteViewController()
+        userDefaultsMangaer = MockUserDefaultsManager()
+        
+        sut = ReviewWritePresenter(viewController: viewController, userDefaultsManager: userDefaultsMangaer)
+    }
 
+    override func tearDown(){
+        sut = nil
+        viewController = nil
+        userDefaultsMangaer = nil
+        
+        super.tearDown()
+    }
+    
 }
